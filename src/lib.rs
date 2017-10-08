0b6cf327a43c4aec6fa64a1b7a7139f714d0281c
Patrick Barrett <patrick@psbarrett.com>
2017-10-08 10:29:35 -0500

    return proper errors

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,8 @@ pub mod message;
 use codec::TplinkSmartHomeCodec;
 use message::*;
 
+use std::io;
+
 use std::time::Duration;
 
 use std::net::SocketAddr;
@@ -24,7 +26,7 @@ use tokio_core::net::UdpSocket;
 use tokio_core::reactor::Core;
 use tokio_timer::Timer;
 
-fn make_request_wait_for_response(request: Message, device_addr: SocketAddr, wait: Duration, count: u64) -> Message {
+fn make_request_wait_for_response(request: Message, device_addr: SocketAddr, wait: Duration, count: u64) -> Result<Message, io::Error> {
     drop(env_logger::init());
 
     let mut core = Core::new().unwrap();
@@ -32,8 +34,8 @@ fn make_request_wait_for_response(request: Message, device_addr: SocketAddr, wai
 
     let src: SocketAddr = "0.0.0.0:0".parse().unwrap();
 
-    let sock = UdpSocket::bind(&src, &handle).unwrap();
-    sock.set_broadcast(true).unwrap();
+    let sock = UdpSocket::bind(&src, &handle)?;
+    sock.set_broadcast(true)?;
 
     let (mut sink, stream) = sock.framed(TplinkSmartHomeCodec).split();
 
@@ -42,7 +44,7 @@ fn make_request_wait_for_response(request: Message, device_addr: SocketAddr, wai
     let timeout = timer.sleep(wait);
 
     println!("<<<<<<<<<<<\n{}", serde_json::to_string(&request).unwrap());
-    sink.start_send((device_addr,Some(request))).unwrap();
+    sink.start_send((device_addr,Some(request)))?;
 
     let mut response = None;
 
@@ -62,28 +64,28 @@ fn make_request_wait_for_response(request: Message, device_addr: SocketAddr, wai
         drop(core.run(sock));
     }
 
-    response.unwrap()
+    response.ok_or(io::Error::new(io::ErrorKind::TimedOut, "timeout waiting for response"))
 }
 
-pub fn get_sysinfo(device_addr: SocketAddr) {
+pub fn get_sysinfo(device_addr: SocketAddr) -> Result<Message, io::Error> {
     let request =
         Message::SystemMsg(
             SystemMsg::GetSysinfo(None)
         );
 
-    make_request_wait_for_response(request, device_addr, Duration::from_secs(3), 1);
+    make_request_wait_for_response(request, device_addr, Duration::from_secs(3), 1)
 }
 
-pub fn get_details(device_addr: SocketAddr) {
+pub fn get_details(device_addr: SocketAddr) -> Result<Message, io::Error> {
     let request =
         Message::LightingServiceMsg(
             LightingServiceMsg::GetLightDetails(None)
         );
 
-    make_request_wait_for_response(request, device_addr, Duration::from_secs(3), 1);
+    make_request_wait_for_response(request, device_addr, Duration::from_secs(3), 1)
 }
 
-pub fn on(device_addr: SocketAddr) {
+pub fn on(device_addr: SocketAddr) -> Result<Message, io::Error> {
     let request =
         Message::LightingServiceMsg(
             LightingServiceMsg::TransitionLightState(
@@ -95,10 +97,10 @@ pub fn on(device_addr: SocketAddr) {
             )
         );
 
-    make_request_wait_for_response(request, device_addr, Duration::from_secs(3), 1);
+    make_request_wait_for_response(request, device_addr, Duration::from_secs(3), 1)
 }
 
-pub fn off(device_addr: SocketAddr) {
+pub fn off(device_addr: SocketAddr) -> Result<Message, io::Error> {
     let request =
         Message::LightingServiceMsg(
             LightingServiceMsg::TransitionLightState(
@@ -110,10 +112,10 @@ pub fn off(device_addr: SocketAddr) {
             )
         );
 
-    make_request_wait_for_response(request, device_addr, Duration::from_secs(3), 1);
+    make_request_wait_for_response(request, device_addr, Duration::from_secs(3), 1)
 }
 
-pub fn hsv(device_addr: SocketAddr, h: u16, s:u8, v: u8) {
+pub fn hsv(device_addr: SocketAddr, h: u16, s:u8, v: u8) -> Result<Message, io::Error> {
     let request =
         Message::LightingServiceMsg(
             LightingServiceMsg::TransitionLightState(
@@ -128,5 +130,5 @@ pub fn hsv(device_addr: SocketAddr, h: u16, s:u8, v: u8) {
             )
         );
 
-    make_request_wait_for_response(request, device_addr, Duration::from_secs(3), 1);
+    make_request_wait_for_response(request, device_addr, Duration::from_secs(3), 1)
 }
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     match app() {
         Ok(()) => (),
         Err(e) => {
-            writeln!(io::stderr(), "error: {:?}", e).unwrap();
+            writeln!(io::stderr(), "error: {}", e).unwrap();
             exit(1);
         }
     }
@@ -47,19 +47,19 @@ fn app() -> Result<(), std::io::Error> {
     let device_addr = SocketAddr::new(device_addr_str.parse().unwrap(), device_port_str.parse().unwrap());
 
     if let Some(_matches) = matches.subcommand_matches("on") {
-        tplink_smart_home::on(device_addr);
+        tplink_smart_home::on(device_addr)?;
     } else if let Some(_matches) = matches.subcommand_matches("off") {
-        tplink_smart_home::off(device_addr);
+        tplink_smart_home::off(device_addr)?;
     } else if let Some(matches) = matches.subcommand_matches("hsv") {
         let h = matches.value_of("HUE").unwrap().parse().unwrap();
         let s = matches.value_of("SATURATION").unwrap().parse().unwrap();
         let v = matches.value_of("VALUE").unwrap().parse().unwrap();
 
-        tplink_smart_home::hsv(device_addr, h, s, v);
+        tplink_smart_home::hsv(device_addr, h, s, v)?;
     } else if let Some(_matches) = matches.subcommand_matches("sysinfo") {
-        tplink_smart_home::get_sysinfo(device_addr);
+        tplink_smart_home::get_sysinfo(device_addr)?;
     } else {
-        tplink_smart_home::get_details(device_addr);
+        tplink_smart_home::get_details(device_addr)?;
     }
 
     Ok(())
